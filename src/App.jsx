00e565920ae11695563f077a864033fb79d62aa1
Abdul Wasay <wasaychohan613@gmail.com>
2026-07-11 09:44:42 -0700

    mobile-padding

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="app">
+    <div className="container">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/features" element={<Features />} />
